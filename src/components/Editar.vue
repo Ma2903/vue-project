5d3dd122d1809d99792d1f9dabeea27087981cea
JP1005YT <godlolpro32@gmail.com>
2024-06-06 09:10:09 -0300

    "link com o bulma e component modulo"

--- src/components/Editar.vue
+++ src/components/Editar.vue
@@ -1,16 +1,25 @@
 <template>
+  <div class="EditScreen">
+    <h2>Editando: {{ element.nome }}</h2>
+    <!-- Renderize o formulário de edição aqui, usando os dados de 'element' -->
     <div>
-      <h2>Editando: {{ element.nome }}</h2>
-      <!-- Renderize o formulário de edição aqui, usando os dados de 'element' -->
-      <div>
-        <p>ID: {{ element.id }}</p>
-        <p>Nome: {{ element.nome }}</p>
-        <p>Payload: {{ element.payload }}</p>
-        <p>Tags: {{ element.tags }}</p>
-        <p>Tipo: {{ element.tipo }}</p>
-      </div>
+      <label for="id">ID:</label>
+      <input type="text" id="id" v-model="element.id" readonly />
+
+      <label for="nome">Nome:</label>
+      <input type="text" id="nome" v-model="element.nome" />
+
+      <label for="payload">Payload:</label>
+      <input type="text" id="payload" v-model="element.payload" />
+
+      <label for="tags">Tags:</label>
+      <input type="text" id="tags" v-model="element.tags" />
+
+      <label for="tipo">Tipo:</label>
+      <input type="text" id="tipo" v-model="element.tipo" />
     </div>
-  </template>
+  </div>
+</template>
   
   
   <script setup>
@@ -24,4 +33,17 @@
   })
   </script>
   
+<style scoped>
+.EditScreen{
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 90%;
+  height: 100%;
+  margin-left: 5%;
+  background: red;
+}
+
+
+</style>
   
